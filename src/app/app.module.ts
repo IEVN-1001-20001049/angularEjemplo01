39c7eb1730710d0dfc4eba5cc45228b366c14899
Miguel Ramírez <20001049@alumnos.utleon.edu.mx>
2023-11-06 18:42:32 -0600

    DMRO 06/11/23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MenuComponent } from './menu/menu/menu.component';
 import { MaterialModule } from './material/material.module';
 import { AgregarComponent } from './ult/agregar/agregar.component';
 import { AppRoutingModule } from './menu/menu/app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import { AppRoutingModule } from './menu/menu/app-routing.module';
     BrowserModule,
     BrowserAnimationsModule,
     MaterialModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
